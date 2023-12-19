3b7da051c4938ecc22f235232c7f092dfb9b126f
Clément COUDRIN <coudrin.clement@gmail.com>
2023-12-19 12:01:15 +0100

    implement signup, corrects css for user profile

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  
+  User: SignUpForm = new SignUpForm("", "", "", "");
+  showErrors : boolean = false;
+
+  constructor() { }
+
+
+  errorCheck( User : SignUpForm): void {
+ 
+  }
+
+  onSubmit(): void {
+    console.log(this.User);
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
+
+export class SignUpForm {
+
+  constructor(
+
+      public firstName: string,
+      public lastName: string,
+      public email: string,
+      public password : string
+
+  ) {}
 
 }
